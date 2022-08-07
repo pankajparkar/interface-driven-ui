4fc28d1e995997b776aeade5a52642bb298e2b27
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-07 23:31:39 +0800

    Angular CLI commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'idu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'interface-driven-ui';
+}
