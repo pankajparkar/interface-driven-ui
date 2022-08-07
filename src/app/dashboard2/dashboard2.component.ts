3a4b077c824bfa07626612af5fc74c24e9f396ca
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-07 23:33:13 +0800

    Added 3 component for dashboard

---
+++ src/app/dashboard2/dashboard2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'idu-dashboard2',
+  templateUrl: './dashboard2.component.html',
+  styleUrls: ['./dashboard2.component.scss']
+})
+export class Dashboard2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
